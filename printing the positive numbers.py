115fe71349d6491337e556597f27c9bc2646d2ef
SURAJ V GHORPADE <65283751+surajghorpade41@users.noreply.github.com>
2020-06-05 11:17:36 -0200

    Add files via upload

---
+++ printing the positive numbers.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[14]:
+
+
+list1=[12,-7,5,64,-14]
+for num in list1: 
+      
+ 
+    if num >= 0: 
+       print(num, end = " ") 
+    
+
+
+# In[13]:
+
+
+list2=[12,14,-95,3]
+for num in list2: 
+      
+ 
+    if num >= 0: 
+       print(num, end = " ") 
+    
+
+
+# In[ ]:
+
+
+
+
